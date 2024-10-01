5cb0736d371d2fb66c9c645992541d752dcf7e90
Patrick Figel <pfigel@greenpeace.org>
2024-10-01 17:18:01 +0200

    GP-44069 Fix DB error when building container prior to upgrader

--- Civi/Sqltasks/CompilerPass.php
+++ Civi/Sqltasks/CompilerPass.php
@@ -20,9 +20,21 @@ class CompilerPass implements CompilerPassInterface {
         E::ts('Run SQL Task'),
       ], []);
 
-      $query = \CRM_Core_DAO::executeQuery("
-        SELECT id FROM civicrm_sqltasks WHERE input_spec IS NOT NULL ORDER BY id ASC;
-      ");
+      try {
+        $errorScope = \CRM_Core_TemporaryErrorScope::useException();
+        $query = \CRM_Core_DAO::executeQuery("
+          SELECT id FROM civicrm_sqltasks WHERE input_spec IS NOT NULL ORDER BY id ASC;
+        ");
+      }
+      catch (\Civi\Core\Exception\DBQueryException $e) {
+        // if this is executed before the upgrader for input_spec runs, civi doesn't boot
+        if (str_contains($e->getMessage(), 'DB Error: no such field')) {
+          // would like to log this, but seems to cause infinite loop in the container build process
+          // \Civi::log()->warning('Unable to add action providers. Please run upgraders to create column civicrm_sqltasks.input_spec');
+          return;
+        }
+        throw $e;
+      }
 
       while ($query->fetch()) {
         $task_id = $query->id;
